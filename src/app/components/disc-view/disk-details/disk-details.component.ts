919fa136330ba974da61cf75ba281c53995f6ed7
Alexandros Spyrou <alexspirou93@gmail.com>
2023-08-15 12:25:43 +0300

    chore: minor changes

--- src/app/components/disc-view/disk-details/disk-details.component.ts
+++ src/app/components/disc-view/disk-details/disk-details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-disk-details',
   templateUrl: './disk-details.component.html',
   styleUrls: ['./disk-details.component.css']
 })
-export class DiskDetailsComponent {
+export class DiskDetailsComponent implements OnInit
+ {
+  id : Number = 0; 
 
+  ngOnInit(): void {
+  }
+
+  onClick(){
+
+    this.id = 3601239;
+  }
 }
